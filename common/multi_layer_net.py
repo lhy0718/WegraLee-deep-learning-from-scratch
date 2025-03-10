85f37f256bf4f7e0918a44437fb7ca2f2afe263e
LEE HANYONG <glhy0718@gmail.com>
2025-03-10 18:44:43 +0900

    ch06 업로드

--- common/multi_layer_net.py
+++ common/multi_layer_net.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import sys, os
+
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 from collections import OrderedDict
@@ -21,8 +22,16 @@ class MultiLayerNet:
         'sigmoid'나 'xavier'로 지정하면 'Xavier 초깃값'으로 설정
     weight_decay_lambda : 가중치 감소(L2 법칙)의 세기
     """
-    def __init__(self, input_size, hidden_size_list, output_size,
-                 activation='relu', weight_init_std='relu', weight_decay_lambda=0):
+
+    def __init__(
+        self,
+        input_size,
+        hidden_size_list,
+        output_size,
+        activation="relu",
+        weight_init_std="relu",
+        weight_decay_lambda=0,
+    ):
         self.input_size = input_size
         self.output_size = output_size
         self.hidden_size_list = hidden_size_list
@@ -34,22 +43,26 @@ class MultiLayerNet:
         self.__init_weight(weight_init_std)
 
         # 계층 생성
-        activation_layer = {'sigmoid': Sigmoid, 'relu': Relu}
+        activation_layer = {"sigmoid": Sigmoid, "relu": Relu}
         self.layers = OrderedDict()
-        for idx in range(1, self.hidden_layer_num+1):
-            self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
-                                                      self.params['b' + str(idx)])
-            self.layers['Activation_function' + str(idx)] = activation_layer[activation]()
+        for idx in range(1, self.hidden_layer_num + 1):
+            self.layers["Affine" + str(idx)] = Affine(
+                self.params["W" + str(idx)], self.params["b" + str(idx)]
+            )
+            self.layers["Activation_function" + str(idx)] = activation_layer[
+                activation
+            ]()
 
         idx = self.hidden_layer_num + 1
-        self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
-            self.params['b' + str(idx)])
+        self.layers["Affine" + str(idx)] = Affine(
+            self.params["W" + str(idx)], self.params["b" + str(idx)]
+        )
 
         self.last_layer = SoftmaxWithLoss()
 
     def __init_weight(self, weight_init_std):
         """가중치 초기화
-        
+
         Parameters
         ----------
         weight_init_std : 가중치의 표준편차 지정（e.g. 0.01）
@@ -59,12 +72,18 @@ class MultiLayerNet:
         all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
         for idx in range(1, len(all_size_list)):
             scale = weight_init_std
-            if str(weight_init_std).lower() in ('relu', 'he'):
-                scale = np.sqrt(2.0 / all_size_list[idx - 1])  # ReLU를 사용할 때의 권장 초깃값
-            elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
-                scale = np.sqrt(1.0 / all_size_list[idx - 1])  # sigmoid를 사용할 때의 권장 초깃값
-            self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx-1], all_size_list[idx])
-            self.params['b' + str(idx)] = np.zeros(all_size_list[idx])
+            if str(weight_init_std).lower() in ("relu", "he"):
+                scale = np.sqrt(
+                    2.0 / all_size_list[idx - 1]
+                )  # ReLU를 사용할 때의 권장 초깃값
+            elif str(weight_init_std).lower() in ("sigmoid", "xavier"):
+                scale = np.sqrt(
+                    1.0 / all_size_list[idx - 1]
+                )  # sigmoid를 사용할 때의 권장 초깃값
+            self.params["W" + str(idx)] = scale * np.random.randn(
+                all_size_list[idx - 1], all_size_list[idx]
+            )
+            self.params["b" + str(idx)] = np.zeros(all_size_list[idx])
 
     def predict(self, x):
         for layer in self.layers.values():
@@ -74,12 +93,12 @@ class MultiLayerNet:
 
     def loss(self, x, t):
         """손실 함수를 구한다.
-        
+
         Parameters
         ----------
         x : 입력 데이터
-        t : 정답 레이블 
-        
+        t : 정답 레이블
+
         Returns
         -------
         손실 함수의 값
@@ -88,27 +107,30 @@ class MultiLayerNet:
 
         weight_decay = 0
         for idx in range(1, self.hidden_layer_num + 2):
-            W = self.params['W' + str(idx)]
-            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W ** 2)
+            W = self.params["W" + str(idx)]
+            weight_decay += (
+                0.5 * self.weight_decay_lambda * np.sum(W**2)
+            )  # L2 Norm을 더하여 가중치 감소를 구현
 
         return self.last_layer.forward(y, t) + weight_decay
 
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        if t.ndim != 1 : t = np.argmax(t, axis=1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
 
     def numerical_gradient(self, x, t):
         """기울기를 구한다(수치 미분).
-        
+
         Parameters
         ----------
         x : 입력 데이터
         t : 정답 레이블
-        
+
         Returns
         -------
         각 층의 기울기를 담은 딕셔너리(dictionary) 변수
@@ -118,9 +140,13 @@ class MultiLayerNet:
         loss_W = lambda W: self.loss(x, t)
 
         grads = {}
-        for idx in range(1, self.hidden_layer_num+2):
-            grads['W' + str(idx)] = numerical_gradient(loss_W, self.params['W' + str(idx)])
-            grads['b' + str(idx)] = numerical_gradient(loss_W, self.params['b' + str(idx)])
+        for idx in range(1, self.hidden_layer_num + 2):
+            grads["W" + str(idx)] = numerical_gradient(
+                loss_W, self.params["W" + str(idx)]
+            )
+            grads["b" + str(idx)] = numerical_gradient(
+                loss_W, self.params["b" + str(idx)]
+            )
 
         return grads
 
@@ -131,7 +157,7 @@ class MultiLayerNet:
         ----------
         x : 입력 데이터
         t : 정답 레이블
-        
+
         Returns
         -------
         각 층의 기울기를 담은 딕셔너리(dictionary) 변수
@@ -152,8 +178,11 @@ class MultiLayerNet:
 
         # 결과 저장
         grads = {}
-        for idx in range(1, self.hidden_layer_num+2):
-            grads['W' + str(idx)] = self.layers['Affine' + str(idx)].dW + self.weight_decay_lambda * self.layers['Affine' + str(idx)].W
-            grads['b' + str(idx)] = self.layers['Affine' + str(idx)].db
+        for idx in range(1, self.hidden_layer_num + 2):
+            grads["W" + str(idx)] = (
+                self.layers["Affine" + str(idx)].dW
+                + self.weight_decay_lambda * self.layers["Affine" + str(idx)].W
+            )
+            grads["b" + str(idx)] = self.layers["Affine" + str(idx)].db
 
         return grads
